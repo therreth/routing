d8b6dfca95b1c69389b8136b2c8fcca297dac1ee
therreth <thenry2@aceproject.space>
2023-01-12 14:26:34 -0600

    50. Finish Course

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,5 +1,22 @@
+import { useContext } from "react";
+import FavoritesContext from "../store/favorites-context";
+import MeetupList from "../components/meetups/MeetupList";
 function FavoritesPage() {
-  return <div>All AllMeetupsPage</div>;
+  const favoritesCtx = useContext(FavoritesContext);
+
+  let content;
+
+  if (favoritesCtx.totalFavorites === 0) {
+    content = <p>"You havent favorited anything"</p>;
+  } else {
+    content = <MeetupList meetups={favoritesCtx.favorites}></MeetupList>;
+  }
+  return (
+    <section>
+      <h1>My Favorties</h1>
+      {content}
+    </section>
+  );
 }
 
 export default FavoritesPage;
