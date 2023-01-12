40ab05956e394b569582ea0ec357bf2bf36049fd
therreth <thenry2@aceproject.space>
2023-01-12 11:37:16 -0600

    43. Sending a post HTTP Request

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -2,7 +2,7 @@ import { useRef } from "react";
 
 import Card from "../ui/Card";
 import classes from "./NewMeetupForm.module.css";
-function NewMeetupForm() {
+function NewMeetupForm(props) {
   const titleInputRef = useRef();
   const imageInputRef = useRef();
   const addressInputRef = useRef();
@@ -20,6 +20,8 @@ function NewMeetupForm() {
       address: enteredAddress,
       description: enteredDescription,
     };
+
+    props.onAddMeetup(meetupData);
   }
 
   return (
--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,10 +1,22 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupsPage() {
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-meetup-site-381c9-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
   return (
     <section>
       <h1>Add new meetup</h1>
-      <NewMeetupForm></NewMeetupForm>
+      <NewMeetupForm onAddMeetup={addMeetupHandler}></NewMeetupForm>
     </section>
   );
 }
