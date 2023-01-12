446e827d43f80326216ff5925654dc81cba4d294
therreth <thenry2@aceproject.space>
2023-01-12 10:37:47 -0600

    38. Adding even more components

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -9,7 +9,11 @@ function MainNavigation() {
         <ul>
           <li>
             <Link to="/">Home</Link>
+          </li>
+          <li>
             <Link to="/new-meetup">Create New meetup</Link>
+          </li>
+          <li>
             <Link to="/favorites">Favorites</Link>
           </li>
         </ul>
